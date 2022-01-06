739e2467ddc487993151aed224ac58437687655e
MahBestBro <76799220+MahBestBro@users.noreply.github.com>
2022-01-06 15:56:03 +1100

    Implemented Cursor and Typing Functionalities
    A cursor is now drawn on the screen, but only at the end of the text. Also had characters repeat when holding down a key (only works for letters, numbers and punctuation; does not work for backspace, enter or tab yet).

--- code/TextEditor.cpp
+++ code/TextEditor.cpp
@@ -1,6 +1,14 @@
+#include <windows.h> 
+
 #include "TextEditor.h"
 #include "TextEditor_input.h"
 #include "TextEditor_font.h"
+
+struct TimedEvent
+{
+    float elapsedTime;
+    float interval;
+};
 
 void DrawBackground(ScreenBuffer* screenBuffer)
 {
@@ -29,7 +37,7 @@ void DrawBackground(ScreenBuffer* screenBuffer)
     }
 }
 
-void DrawText(ScreenBuffer* screenBuffer, FontChar fontChars[128], char* text, int xCoord, int yCoord)
+int DrawText(ScreenBuffer* screenBuffer, FontChar fontChars[128], char* text, int xCoord, int yCoord)
 {
     char* at = text;
 	int advance = 0;
@@ -41,7 +49,7 @@ void DrawText(ScreenBuffer* screenBuffer, FontChar fontChars[128], char* text, i
         int glyphHeight = (int)fc.height;
 
         int xOffset = fc.left + advance + xCoord;
-        int yOffset = ((fc.height - fc.top) + yCoord) * screenBuffer->width;
+        int yOffset = (yCoord - (fc.height - fc.top)) * screenBuffer->width;
         
         Assert(xOffset < screenBuffer->width);
         Assert(yOffset < screenBuffer->width * screenBuffer->height);
@@ -68,12 +76,93 @@ void DrawText(ScreenBuffer* screenBuffer, FontChar fontChars[128], char* text, i
 		advance += fc.advance / 64;
     }
 
-	int test = 0;
+	return advance;
 }
 
-void Draw(ScreenBuffer* screenBuffer, FontChar fontChars[128], int xOffset, int yOffset)
+void DrawCursor(ScreenBuffer* screenBuffer, int xCoord, int yCoord)
 {
-    DrawBackground(screenBuffer);
+    int cursorWidth = 2;
+    int cursorHeight = 13;
 
-    DrawText(screenBuffer, fontChars, "ur mom\t", 300, 300);
+    int xPos = xCoord;
+    int yPos = (yCoord * screenBuffer->width);
+
+    byte* row = (byte*)screenBuffer->memory + (xPos + yPos) * PIXEL_IN_BYTES;
+    for (int y = 0; y < cursorHeight; ++y)
+    {
+        byte* pixel = row;
+        for (int x = 0; x < cursorWidth; ++x)
+        {
+            *pixel = 0;
+            pixel++;
+            *pixel = 255;
+            pixel++;
+            *pixel = 0;
+            pixel++;
+            *pixel = 0;
+            pixel++;
+        }
+
+        row += screenBuffer->width * PIXEL_IN_BYTES;
+    } 
+}
+
+char currentChar = 0;
+//TODO: Make this dynamic (or change into lines)
+char text[256];
+int numChars = 0;
+
+TimedEvent cursorBlink = {0.0f, 0.5f};
+TimedEvent holdChar = {0.0f, 0.5f};
+TimedEvent repeatChar = {0.0f, 0.02f};
+
+void Draw(ScreenBuffer* screenBuffer, FontChar fontChars[128], Input* input, float dt)
+{
+    cursorBlink.elapsedTime += dt;
+
+    bool typing = false;
+    for (int code = (int)INPUTCODE_SPACE; code < (int)NUM_INPUTS; ++code)
+    {
+        if (InputDown(input->flags[code]))
+        {
+            char charOfKeyPressed = InputCodeToChar((InputCode)code, InputHeld(input->leftShift));
+            text[numChars++] = charOfKeyPressed;
+            currentChar = charOfKeyPressed;
+        }
+        else if (InputHeld(input->flags[code]))
+            typing = true;
+    }
+
+    if (!typing)
+    {
+        numChars -= InputDown(input->backspace);
+        text[numChars] = 0;
+    }
+
+    DrawBackground(screenBuffer);
+    int cursorX = DrawText(screenBuffer, fontChars, text, 25, 300);
+
+    if (!typing && cursorBlink.elapsedTime >= cursorBlink.interval)
+    {
+        holdChar.elapsedTime = 0.0f;
+        if (cursorBlink.elapsedTime >= 2 * cursorBlink.interval) 
+            cursorBlink.elapsedTime = 0.0f;
+    }
+    else
+    {
+        DrawCursor(screenBuffer, 25 + cursorX, 300);
+        if (typing)
+        {
+            holdChar.elapsedTime += dt;
+            if (holdChar.elapsedTime >= holdChar.interval)
+            {
+                repeatChar.elapsedTime += dt;
+                if (repeatChar.elapsedTime >= repeatChar.interval)
+                {
+                    text[numChars++] = currentChar;
+                    repeatChar.elapsedTime = 0.0f;
+                }
+            }
+        }
+    }
 }
