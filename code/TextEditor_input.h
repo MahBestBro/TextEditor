3bc89f05f8a1c0447908c2fca8f6201e967d833b
MahBestBro <76799220+MahBestBro@users.noreply.github.com>
2022-01-06 16:02:10 +1100

    Swapped Ordering of Space
    Placed the space input flags right before all the "shiftable" keys just cause it was a little neater to do so for code in TextEditor.ccp's Draw function.

--- code/TextEditor_input.h
+++ code/TextEditor_input.h
@@ -1,4 +1,4 @@
-#include "TextEditor.h"
+#include "TextEditor_defs.h"
 
 #ifndef TEXT_EDITOR_INPUT_H
 #define TEXT_EDITOR_INPUT_H
@@ -18,7 +18,6 @@ enum InputCode
     INPUTCODE_UP,
     INPUTCODE_DOWN,
 
-    INPUTCODE_SPACE,
     INPUTCODE_BACKSPACE,
     INPUTCODE_ENTER,
     INPUTCODE_LSHIFT,
@@ -26,6 +25,7 @@ enum InputCode
     INPUTCODE_LCTRL,
     INPUTCODE_LALT,
     INPUTCODE_CAPSLOCK,
+    INPUTCODE_SPACE,
 
     INPUTCODE_A,
     INPUTCODE_B,
@@ -82,90 +82,13 @@ enum InputCode
 };
 
 #define MOUSE_START INPUTCODE_LEFT_MOUSE
-#define ARROWS_START INPUTCODE_UP
-#define NON_SHIFT_START INPUTCODE_SPACE
+#define ARROWS_START INPUTCODE_LEFT
+#define NON_SHIFT_START INPUTCODE_BACKSPACE
 #define SHIFT_START INPUTCODE_A
 #define LETTER_START INPUTCODE_A
 #define NUMBER_START INPUTCODE_0
 #define PUNCTUATION_START INPUTCODE_OPEN_SQ_BRACKET
-#define OTHER_SYMBOLS_START INPUTCODE_BACKSLASH
-
-inline char* InputCodeToStr(InputCode code)
-{
-    switch (code)
-    {
-        case INPUTCODE_LEFT_MOUSE:  return "LEFT MOUSE";
-        case INPUTCODE_RIGHT_MOUSE:  return "RIGHT MOUSE";
-        case INPUTCODE_MIDDLE_MOUSE: return "MIDDLE MOUSE";
-    
-        case INPUTCODE_LEFT:  return "LEFT";
-        case INPUTCODE_RIGHT: return "RIGHT";
-        case INPUTCODE_UP:    return "UP";
-        case INPUTCODE_DOWN:  return "DOWN";
-
-        case INPUTCODE_SPACE:     return "SPACE";
-        case INPUTCODE_BACKSPACE: return "BACKSPACE";
-        case INPUTCODE_ENTER:     return "ENTER";
-        case INPUTCODE_LSHIFT:    return "LSHIFT";
-        case INPUTCODE_TAB:       return "TAB";
-        case INPUTCODE_LCTRL:     return "LCTRL";
-        case INPUTCODE_LALT:      return "LALT";
-        case INPUTCODE_CAPSLOCK:  return "CAPSLOCK";
-
-        case INPUTCODE_A: return "A";
-        case INPUTCODE_B: return "B";
-        case INPUTCODE_C: return "C";
-        case INPUTCODE_D: return "D";
-        case INPUTCODE_E: return "E";
-        case INPUTCODE_F: return "F";
-        case INPUTCODE_G: return "G";
-        case INPUTCODE_H: return "H";
-        case INPUTCODE_I: return "I";
-        case INPUTCODE_J: return "J";
-        case INPUTCODE_K: return "K";
-        case INPUTCODE_L: return "L";
-        case INPUTCODE_M: return "M";
-        case INPUTCODE_N: return "N";
-        case INPUTCODE_O: return "O";
-        case INPUTCODE_P: return "P";
-        case INPUTCODE_Q: return "Q";
-        case INPUTCODE_R: return "R";
-        case INPUTCODE_S: return "S";
-        case INPUTCODE_T: return "T";
-        case INPUTCODE_U: return "U";
-        case INPUTCODE_V: return "V";
-        case INPUTCODE_W: return "W";
-        case INPUTCODE_X: return "X";
-        case INPUTCODE_Y: return "Y";
-        case INPUTCODE_Z: return "Z";
-
-        case INPUTCODE_0: return "0";
-        case INPUTCODE_1: return "1";
-        case INPUTCODE_2: return "2";
-        case INPUTCODE_3: return "3";
-        case INPUTCODE_4: return "4";
-        case INPUTCODE_5: return "5";
-        case INPUTCODE_6: return "6";
-        case INPUTCODE_7: return "7";
-        case INPUTCODE_8: return "8";
-        case INPUTCODE_9: return "9";
-
-        case INPUTCODE_OPEN_SQ_BRACKET:   return "OPEN SQ BRACKET";
-        case INPUTCODE_CLOSED_SQ_BRACKET: return "CLOSED SQ BRACKET";
-        case INPUTCODE_SEMICOLON:         return "SEMICOLON";
-        case INPUTCODE_QUOTE:             return "QUOTE";
-        case INPUTCODE_COMMA:             return "COMMA";
-        case INPUTCODE_PERIOD:            return "PERIOD";
-        case INPUTCODE_FORWARD_SLASH:     return "FORWARD SLASH";
-        
-        case INPUTCODE_BACK_SLASH: return "BACK SLASH";
-        case INPUTCODE_MINUS:      return "MINUS";
-        case INPUTCODE_EQUALS:     return "EQUALS";
-        case INPUTCODE_GRAVE:      return "GRAVE";
-
-        default: return "NULL";
-    }
-}
+#define OTHER_SYMBOLS_START INPUTCODE_BACK_SLASH
 
 union Input
 {
@@ -187,7 +110,6 @@ union Input
             byte arrowKeys[4];
         };  
 
-        byte space;
         byte backspace;
         byte enter;
         byte leftShift;
@@ -195,6 +117,7 @@ union Input
         byte leftCtrl;
         byte leftAlt;
         byte capsLock;
+        byte space;
 
         byte letterKeys[26];
         byte numberKeys[10];
@@ -207,6 +130,9 @@ union Input
     };
     byte flags[NUM_INPUTS];
 };
+
+char* InputCodeToStr(InputCode code);
+char InputCodeToChar(InputCode code, bool shift);
 
 inline bool InputDown(byte inputFlags)
 {
