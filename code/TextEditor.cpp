d5e4f471a215ae2bc731f26cc40b859f76407d7c
MahBestBro <76799220+MahBestBro@users.noreply.github.com>
2022-01-07 15:15:44 +1100

    Enter and Tab implemented, Backspace fix and Refactor.
    Made enter and tab produce characters. Tab will need to be changed so that it aligns via the "tab lines" instead of doing 4 spaces every time. Enter may also change in the future if I decide to split the characters into lines. Also extrapolated the draw functions into "DrawRect" and "DrawPixels8bpp" just so I don't need to copy and paste doubly nested for loops.

--- code/TextEditor.cpp
+++ code/TextEditor.cpp
@@ -2,81 +2,143 @@
 
 #include "TextEditor.h"
 #include "TextEditor_input.h"
-#include "TextEditor_font.h"
 
 struct TimedEvent
 {
     float elapsedTime;
     float interval;
+    void (*OnTrigger)(void);
 };
 
-void DrawBackground(ScreenBuffer* screenBuffer)
+struct IntPair
 {
-    int width = screenBuffer->width;
-    int height = screenBuffer->height;
+    int x, y;
+};
 
-    int pitch = width * PIXEL_IN_BYTES;
-    byte* row = (byte*)screenBuffer->memory;
-    for (int y = 0; y < height; ++y)
+void HandleTimedEvent(TimedEvent* timedEvent, float dt, TimedEvent* nestedEvent)
+{
+    timedEvent->elapsedTime += dt;
+    if (timedEvent->OnTrigger) timedEvent->OnTrigger(); //May not be needed???
+    if (timedEvent->elapsedTime >= timedEvent->interval)
     {
-        byte* pixel = (byte*)row;
-        for (int x = 0; x < width; ++x)
+        if (nestedEvent)
         {
-            //Pixel in memory: 0xBBGGRRxx
-            *pixel = 255;
+            nestedEvent->elapsedTime += dt;
+            if (nestedEvent->elapsedTime >= nestedEvent->interval)
+            {
+                if (nestedEvent->OnTrigger) nestedEvent->OnTrigger();
+                nestedEvent->elapsedTime = 0.0f;
+            }
+        }
+    }
+}
+
+
+void DrawRect(ScreenBuffer* screenBuffer, 
+              int left, int right, int bottom, int top, 
+              byte r, byte g, byte b)
+{
+    //TODO: Update asserts to make sure width and height do not go offscreen
+    Assert(left < screenBuffer->width);
+    Assert(top < screenBuffer->width * screenBuffer->height);
+
+    int drawWidth = right - left;
+    int drawHeight = top - bottom;
+
+    int start = (left + bottom * screenBuffer->width) * PIXEL_IN_BYTES;
+	byte* row = (byte*)screenBuffer->memory + start;
+	for (int y = 0; y < drawHeight; ++y)
+    {
+        byte* pixel = row;
+        for (int x = 0; x < drawWidth; ++x)
+        {
+            *pixel = b;
             pixel++;
-            *pixel = 255;
+            *pixel = g;
             pixel++;
-            *pixel = 255;
+            *pixel = r;
             pixel++;
             *pixel = 0;
             pixel++;
         }
-
-        row += pitch;
+		row += screenBuffer->width * PIXEL_IN_BYTES;
     }
 }
 
-int DrawText(ScreenBuffer* screenBuffer, FontChar fontChars[128], char* text, int xCoord, int yCoord)
+void Draw8bppPixels(ScreenBuffer* screenBuffer, 
+                    int left, int right, int bottom, int top, 
+                    byte* pixels)
+{
+    //TODO: Update asserts to make sure width and height do not go offscreen
+    Assert(left < screenBuffer->width);
+    Assert(top < screenBuffer->width * screenBuffer->height);
+
+    int drawWidth = right - left;
+    int drawHeight = top - bottom;
+
+    int start = (left + bottom * screenBuffer->width) * PIXEL_IN_BYTES;
+	byte* row = (byte*)screenBuffer->memory + start;
+	for (int y = drawHeight - 1; y >= 0; --y)
+    {
+        byte* pixel = row;
+        for (int x = 0; x < drawWidth; ++x)
+        {
+            byte* drawnPixel = pixels + drawWidth * y + x;
+            *pixel = 255 - *drawnPixel;
+            pixel++;
+            *pixel = 255 - *drawnPixel;
+            pixel++;
+            *pixel = 255 - *drawnPixel;
+            pixel++;
+            *pixel = 255 - *drawnPixel;
+            pixel++;
+        }
+		row += screenBuffer->width * PIXEL_IN_BYTES;
+    }
+}
+
+IntPair DrawText(ScreenBuffer* screenBuffer, FontChar fontChars[128], char* text, 
+                 int xCoord, int yCoord)
 {
     char* at = text;
-	int advance = 0;
+	int xAdvance = 0;
+    int yAdvance = 0;
     for (; at[0]; at++)
     {
-        FontChar fc = fontChars[at[0]];
-
-        int glyphWidth = (int)fc.width;
-        int glyphHeight = (int)fc.height;
-
-        int xOffset = fc.left + advance + xCoord;
-        int yOffset = (yCoord - (fc.height - fc.top)) * screenBuffer->width;
-        
-        Assert(xOffset < screenBuffer->width);
-        Assert(yOffset < screenBuffer->width * screenBuffer->height);
-		byte* row = (byte*)screenBuffer->memory + (xOffset + yOffset) * PIXEL_IN_BYTES;
-		for (int y = glyphHeight - 1; y >= 0; --y)
+		if (at[0] == '\n')
         {
-            byte* pixel = row;
-            for (int x = 0; x < glyphWidth; ++x)
-            {
-                byte* glyphPixel = (byte*)fc.pixels + fc.width * y + x;
-                *pixel = 255 - *glyphPixel;
-                pixel++;
-                *pixel = 255 - *glyphPixel;
-                pixel++;
-                *pixel = 255 - *glyphPixel;
-                pixel++;
-                *pixel = 255 - *glyphPixel;
-                pixel++;
-                
-            }
-			row += screenBuffer->width * PIXEL_IN_BYTES;
+            yAdvance += FONT_SIZE;
+            xAdvance = 0;
         }
-
-		advance += fc.advance / 64;
+        else if (at[0] == '\t')
+        {
+            FontChar fc = fontChars[' '];
+            int xOffset = fc.left + xAdvance + xCoord;
+            int yOffset = yCoord - (fc.height - fc.top) - yAdvance;
+            //TODO: change this so that it aligns with tabs
+            for (int _ = 0; _ < 4; ++_)
+            {
+                Draw8bppPixels(screenBuffer, 
+                               xOffset, xOffset + fc.width, yOffset, yOffset + fc.height,
+                               (byte*)fc.pixels);
+                xOffset += fc.left;
+            }
+            xAdvance += 4 * (fc.advance / 64);
+        }
+		else
+		{
+            FontChar fc = fontChars[at[0]];
+            int xOffset = fc.left + xAdvance + xCoord;
+            int yOffset = yCoord - (fc.height - fc.top) - yAdvance;
+            Draw8bppPixels(screenBuffer, 
+                           xOffset, xOffset + fc.width, yOffset, yOffset + fc.height,
+                           (byte*)fc.pixels);
+		    xAdvance += fc.advance / 64;
+		}
+        
     }
 
-	return advance;
+	return {xAdvance, -yAdvance};
 }
 
 void DrawCursor(ScreenBuffer* screenBuffer, int xCoord, int yCoord)
@@ -112,37 +174,70 @@ char currentChar = 0;
 char text[256];
 int numChars = 0;
 
-TimedEvent cursorBlink = {0.0f, 0.5f};
-TimedEvent holdChar = {0.0f, 0.5f};
-TimedEvent repeatChar = {0.0f, 0.02f};
+void AddChar()
+{
+    text[numChars] = currentChar;
+    numChars += (numChars < 255);
+}
+
+void RemoveChar()
+{
+    numChars -= (numChars > 0);
+    text[numChars] = 0;
+}
+
+TimedEvent cursorBlink = {0.0f, 0.5f, 0};
+TimedEvent holdChar = {0.0f, 0.5f, 0};
+TimedEvent repeatChar = {0.0f, 0.02f, &AddChar};
+TimedEvent holdBackspace = {0.0f, 0.5f, 0};
+TimedEvent repeatBackspace = {0.0f, 0.02f, &RemoveChar};
+
+bool capslockOn = false;
 
 void Draw(ScreenBuffer* screenBuffer, FontChar fontChars[128], Input* input, float dt)
 {
-    cursorBlink.elapsedTime += dt;
-
-    bool typing = false;
+    bool charKeyPressed = false;
     for (int code = (int)INPUTCODE_SPACE; code < (int)NUM_INPUTS; ++code)
     {
         if (InputDown(input->flags[code]))
         {
-            char charOfKeyPressed = InputCodeToChar((InputCode)code, InputHeld(input->leftShift));
-            text[numChars++] = charOfKeyPressed;
+            char charOfKeyPressed = InputCodeToChar(
+                (InputCode)code, 
+                InputHeld(input->leftShift), 
+                capslockOn);
             currentChar = charOfKeyPressed;
+            AddChar();
         }
         else if (InputHeld(input->flags[code]))
-            typing = true;
+            charKeyPressed = true;
     }
 
-    if (!typing)
+    if (charKeyPressed)
     {
-        numChars -= InputDown(input->backspace);
-        text[numChars] = 0;
+        HandleTimedEvent(&holdChar, dt, &repeatChar);
+    }
+    else
+    {
+        holdChar.elapsedTime = 0.0f;
+
+        if (InputDown(input->backspace))
+            RemoveChar();
+        else if (InputHeld(input->backspace))
+            HandleTimedEvent(&holdBackspace, dt, &repeatBackspace);  
+        else 
+            holdBackspace.elapsedTime = 0.0f;
+
+        if (InputDown(input->capsLock))
+            capslockOn = !capslockOn;
     }
 
-    DrawBackground(screenBuffer);
-    int cursorX = DrawText(screenBuffer, fontChars, text, 25, 300);
+    //Draw Background;
+    DrawRect(screenBuffer, 0, screenBuffer->width, 0, screenBuffer->height, 255, 255, 255);
+    IntPair cursorPos = DrawText(screenBuffer, fontChars, text, 25, 300);
 
-    if (!typing && cursorBlink.elapsedTime >= cursorBlink.interval)
+    cursorBlink.elapsedTime += dt;
+    if ((!charKeyPressed && !InputHeld(input->backspace)) && 
+        cursorBlink.elapsedTime >= cursorBlink.interval)
     {
         holdChar.elapsedTime = 0.0f;
         if (cursorBlink.elapsedTime >= 2 * cursorBlink.interval) 
@@ -150,19 +245,6 @@ void Draw(ScreenBuffer* screenBuffer, FontChar fontChars[128], Input* input, flo
     }
     else
     {
-        DrawCursor(screenBuffer, 25 + cursorX, 300);
-        if (typing)
-        {
-            holdChar.elapsedTime += dt;
-            if (holdChar.elapsedTime >= holdChar.interval)
-            {
-                repeatChar.elapsedTime += dt;
-                if (repeatChar.elapsedTime >= repeatChar.interval)
-                {
-                    text[numChars++] = currentChar;
-                    repeatChar.elapsedTime = 0.0f;
-                }
-            }
-        }
+        DrawCursor(screenBuffer, 25 + cursorPos.x, 300 + cursorPos.y);
     }
 }
