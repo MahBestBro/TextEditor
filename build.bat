1d8ef34b8d7770d0dbc6e2a277872dbe145581c8
MahBestBro <76799220+MahBestBro@users.noreply.github.com>
2022-02-24 23:16:13 +1100

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ set commonCompilerFlags= -nologo -MDd -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -w
 set commonLinkerFlags= -incremental:no -opt:ref  
 set libraries= user32.lib gdi32.lib shell32.lib Comdlg32.lib freetyped.lib
 set includeDirs= includes
-set cppFiles= code\TextEditor_win32.cpp code\TextEditor.cpp code\TextEditor_input.cpp code\TextEditor_string.cpp
+set cppFiles= code\TextEditor_win32.cpp code\TextEditor.cpp code\TextEditor_input.cpp code\TextEditor_string.cpp code\TextEditor_config.cpp
 
 rem -GR- and -EHa- turn off exception handling stuff
 rem -Oi turns on commpiler intrisics (replaces c functions if it knows how to do it in assembly)
@@ -19,20 +19,24 @@ rem -FC uses full path for error and warning messages
 rem IGNORED WARNINGS:
 rem - 4100: Unused function parameters (does not work with winmain or win32 callback)
 rem - 4201: non-standard anonymous struct or union (we use them) 
+ 
 
-mkdir 
-pushd 
-
-rem Debug build
+mkdir config_code_generator 
 cls
+pushd config_code_generator 
+
+rem Config Preprocessor
 del *.pdb > NUL 2> NUL
-cl  %commonCompilerFlags% /EHsc %cppFiles% /I %includeDirs% /link %commonLinkerFlags% -libpath:"lib" %libraries% /out:TextEditor.exe 
-
-
-rem version Names
-rem /out:TCtetris_1.0.exe 
+cl  %commonCompilerFlags% /EHsc ../code/TextEditor_config_code_generator.cpp /I %includeDirs% /link %commonLinkerFlags% -libpath:"../lib" %libraries% /out:TextEditor_config_code_generator.exe
+TextEditor_config_code_generator.exe
 
 popd
+
+rem Debug build
+del *.pdb > NUL 2> NUL
+cl  %commonCompilerFlags% /EHsc %cppFiles% /I %includeDirs% /link %commonLinkerFlags% -libpath:"lib" %libraries% /out:TextEditor.exe  
+
+
 
 rem /EHsc adds in error handler
 rem /LD builds as DLL when using "cd" command
