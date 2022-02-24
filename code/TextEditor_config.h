676611d54cbe1d01d943138608701b3d0d1cad95
MahBestBro <76799220+MahBestBro@users.noreply.github.com>
2022-02-24 23:08:42 +1100

    Update TextEditor_config.h
    It Just contains the function declaration now lol.

--- code/TextEditor_config.h
+++ code/TextEditor_config.h
@@ -1,29 +1,8 @@
-#include "TextEditor_defs.h"
-#include "TextEditor.h"
-#include "TextEditor_string.h"
+#ifndef TEXT_EDITOR_CONFIG_H
+#define TEXT_EDITOR_CONFIG_H
 
-#define StructOffset(struct, member) (uint32)&((struct*)0)->member
-
-struct UserSettingsType
-{
-    COLOUR_RGB,
-    COLOUR_RGBA,
-    INT,
-    BOOL,
-    STRING
-};
-
-struct UserSettingsMemberMetaData
-{
-    UserSettingsType type;
-    uint32 offset;
-};
-
-UserSettingsMemberMetaData memberMetaData[] = 
-{
-    {COLOUR_RGB, StructOffset(UserSettings, member)},
-};
-
-#include "TextEditor_user_settings.inc"
+#include "TextEditor_user_settings_struct.inc"
 
 UserSettings LoadUserSettingsFromConfigFile();
+
+#endif
