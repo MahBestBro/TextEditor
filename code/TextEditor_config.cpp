4afe7120c755525e239a02e764c01d08da49c55c
MahBestBro <76799220+MahBestBro@users.noreply.github.com>
2022-02-24 23:08:01 +1100

    Colour implementation
    Colours can be parsed. Have yet to do other variable types and will add them once needed.

--- code/TextEditor_config.cpp
+++ code/TextEditor_config.cpp
@@ -1,26 +1,129 @@
 #include "TextEditor_defs.h"
 #include "TextEditor.h"
 #include "TextEditor_string.h"
+#include "TextEditor_meta.h"
 #include "TextEditor_config.h"
+
+#include "TextEditor_user_settings_memberData.inc"
+
+char* AdvanceOnePastChar(char* str, char target, __Out int* len = 0)
+{
+    int resultLen = 0;
+    while (str[0] && str[0] != target)
+    {
+        str++;
+        resultLen++;
+    }
+    if (str[0]) str++;
+ 
+    if (len) *len = resultLen;
+    return str;
+}
+
+char* AdvanceToCharAndGetString(char* str, char target, __Out int* len)
+{
+    char* result = str;
+    int resultLen = 0;
+    while (str[0] && str[0] != target)
+    {
+        str++;
+        resultLen++;
+    }
+ 
+    *len = resultLen;
+    return result;
+}
 
 UserSettings LoadUserSettingsFromConfigFile()
 {
-    UserSettings result;
+    UserSettings result = {};
 
     char* file = ReadEntireFile("config/config_general.txt");
 
-    char* line = AdvanceToNextLine(file);
-    while (file != nullptr)
+    int numLines = 0;
+    char** lines = SplitStringByLines(file, &numLines);
+    for (int i = 0; i < numLines; ++i)
     {
-        int nameLen = 0;
-        char* name = SplitString(line, ' ', &lineLen);
+        UserSettingsMemberMetaData memberData = memberMetaData[i];
+        char* val = AdvanceOnePastChar(lines[i], ' ');
 
-        if (CompareStrings(name, nameLen, "backgroundColour")) 
+        switch(memberData.type)
+        {
+            case TYPE_COLOUR_RGB:
+            {
+                Colour colour;
 
+                int offset = 0;
+                bool success = true;
 
+                char* rStr = AdvanceToCharAndGetString(val, ',', &offset);
+                byte r = StringToByte(rStr, offset, &success);
+                Assert(success);
+                colour.r = r;
 
-        line = AdvanceToNextLine(line);
+                char* gStr = AdvanceToCharAndGetString(rStr + offset + 1, ',', &offset);
+                byte g = StringToByte(gStr, offset, &success);
+                Assert(success);
+                colour.g = g;
+
+                char* bStr = AdvanceToCharAndGetString(gStr + offset + 1, ',', &offset);
+                byte b = StringToByte(bStr, offset, &success);
+                Assert(success);
+                colour.b = b;
+
+                *(Colour*)MemberPtr(result, memberData.offset) = colour;
+            } break;
+
+            case TYPE_COLOUR_RGBA:
+            {
+                ColourRGBA colour;
+
+                int offset = 0;
+                bool success = true;
+
+                char* rStr = AdvanceToCharAndGetString(val, ',', &offset);
+                byte r = StringToByte(rStr, offset, &success);
+                Assert(success);
+                colour.r = r;
+
+                char* gStr = AdvanceToCharAndGetString(rStr + offset + 1, ',', &offset);
+                byte g = StringToByte(gStr, offset, &success);
+                Assert(success);
+                colour.g = g;
+
+                char* bStr = AdvanceToCharAndGetString(gStr + offset + 1, ',', &offset);
+                byte b = StringToByte(bStr, offset, &success);
+                Assert(success);
+                colour.b = b;
+
+                char* aStr = AdvanceToCharAndGetString(bStr + offset + 1, ',', &offset);
+                byte a = StringToByte(aStr, offset, &success);
+                Assert(success);
+                colour.a = a;
+
+                *(ColourRGBA*)MemberPtr(result, memberData.offset) = colour;
+            } break;
+
+            case TYPE_INT:
+            {
+                UNIMPLEMENTED("TODO: Implement once needed")
+            } break;
+
+            case TYPE_BOOL:
+            {
+                UNIMPLEMENTED("TODO: Implement once needed")
+            } break;
+
+            case TYPE_STRING:
+            {
+                UNIMPLEMENTED("TODO: Implement once needed")
+            } break;
+        }
     }
+
+    for (int i = 0; i < numLines; ++i)
+        free(lines[i]);
+    free(lines);
 
     return result;
 }
