be78ff2f1764a728e0d0e80dbcf560f1cded8e08
MahBestBro <76799220+MahBestBro@users.noreply.github.com>
2022-02-24 15:09:22 +1100

    Original Build File
    Had put off uploading this but now it's starting to change a bit so maybe do wanna keep history

---
+++ build.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set commonCompilerFlags= -nologo -MDd -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -FC -Z7 
+set commonLinkerFlags= -incremental:no -opt:ref  
+set libraries= user32.lib gdi32.lib shell32.lib Comdlg32.lib freetyped.lib
+set includeDirs= includes
+set cppFiles= code\TextEditor_win32.cpp code\TextEditor.cpp code\TextEditor_input.cpp code\TextEditor_string.cpp
+
+rem -GR- and -EHa- turn off exception handling stuff
+rem -Oi turns on commpiler intrisics (replaces c functions if it knows how to do it in assembly)
+rem -Z7 and -Zi are for debugging. -Z7 does not produce vc140.pdb
+rem -nologo gets rid of compiler logo
+rem -MT gets rid of multithread dll bug, is used for release builds
+rem -Gm- turns off minimal rebuild
+rem -Od turns off all optimisation
+rem -incremental:no turns off incremental build
+rem -FC uses full path for error and warning messages
+
+rem IGNORED WARNINGS:
+rem - 4100: Unused function parameters (does not work with winmain or win32 callback)
+rem - 4201: non-standard anonymous struct or union (we use them) 
+
+mkdir 
+pushd 
+
+rem Debug build
+cls
+del *.pdb > NUL 2> NUL
+cl  %commonCompilerFlags% /EHsc %cppFiles% /I %includeDirs% /link %commonLinkerFlags% -libpath:"lib" %libraries% /out:TextEditor.exe 
+
+
+rem version Names
+rem /out:TCtetris_1.0.exe 
+
+popd
+
+rem /EHsc adds in error handler
+rem /LD builds as DLL when using "cd" command
+rem /PDB:(name).pdb creates a pdb with the file name
+rem %date:~-4,4%%date:~-10,2%%date:~-7,2% is noise for visual studio pdb bypassing
